a383d596cad0f236ec6d49614cb0211a2990592f
Telewa Emmanuel <emmanuelt2009@gmail.com>
2019-08-08 03:50:16 +0300

    remove secret from repo (#3)
    * remove secret from repo

--- backend/run_test_parallel.sh
+++ backend/run_test_parallel.sh
@@ -4,4 +4,3 @@ coverage erase
 coverage run --parallel-mode --concurrency=multiprocessing --rcfile=./.coveragerc manage.py test --settings=configuration.settings -v 3 --parallel=3
 coverage combine
 coverage report -m
-coveralls
